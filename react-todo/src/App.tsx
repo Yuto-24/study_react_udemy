b87193637b02e90486ac48357e5e712408eefbad
mac <mac@ots>
2023-09-12 16:10:56 +0900

    add function for 'todo'

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -2,33 +2,72 @@ import { useState } from "react";
 import "./styles.css"
 
 export const App = () => {
-  const [incompleteTodos, setIncompleteTodos] = useState([
+  const [todoText, setTodoText] = useState('');
+
+  const [incompleteTasks, setIncompleteTasks] = useState([
     'ああああ',
     'いいいいい'
   ]);
 
-  const [completeTodos, setCompleteTodos] = useState([
+  const [completeTasks, setCompleteTasks] = useState([
     '完了済みタスク',
     'うううう'
   ]);
 
+  const onChangeTodoText = (event: any) => setTodoText(event.target.value);
+
+  const addTask = (taskText = '', isIncomplete = true) => {
+    if (taskText === '') return;
+
+    if (isIncomplete === true) {
+      const newTasks = [...incompleteTasks, taskText];
+      setIncompleteTasks(newTasks);
+    } else {
+      const newTasks = [...completeTasks, taskText];
+      setCompleteTasks(newTasks);
+    }
+    setTodoText('');
+  }
+
+  const removeTask = (index: number, isIncomplete = true) => {
+    if (isIncomplete) {
+      const newTasks = [...incompleteTasks];
+      newTasks.splice(index, 1);
+      setIncompleteTasks(newTasks);
+    } else {
+      const newTasks = [...completeTasks];
+      newTasks.splice(index, 1);
+      setCompleteTasks(newTasks);
+    }
+  }
+
+  const onClickComplete = (index: number) => {
+    removeTask(index);
+    addTask(incompleteTasks[index], false);
+  }
+
+  const onClickReturn = (index: number) => {
+    removeTask(index, false);
+    addTask(completeTasks[index], true);
+  }
+
   return (
     <>
       <div className="input-area">
-        <input type="text" placeholder="TODOを入力" />
-        <button>追加</button>
+        <input type="text" placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText} />
+        <button onClick={() => addTask(todoText)}>追加</button>
       </div>
 
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
 
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTasks.map((todo, index) => {
             return (
               <li key={todo} className="list-row">
                 <p>{todo}</p>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
+                <button onClick={() => removeTask(index)}>削除</button>
               </li>
             );
           })}
@@ -40,12 +79,11 @@ export const App = () => {
         <p className="title">完了済のTODO</p>
 
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTasks.map((todo, index) => {
             return (
               <li key={todo} className="list-row">
                 <p>{todo}</p>
-                <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickReturn(index)}>戻す</button>
               </li>
             );
           })}
